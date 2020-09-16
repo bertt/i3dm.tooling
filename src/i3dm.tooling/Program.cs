a057d6d8c15ff769344b2b2d510aa9b7b7751cc6
bertt <btemme@hotmail.com>
2020-09-16 12:01:03 +0200

    updating sharpgltf

--- src/i3dm.tooling/Program.cs
+++ src/i3dm.tooling/Program.cs
@@ -47,13 +47,13 @@ namespace i3dm.tooling
             Console.WriteLine("i3dm header version: " + i3dm.I3dmHeader.Version);
             Console.WriteLine("i3dm GltfFormat: " + i3dm.I3dmHeader.GltfFormat);
             Console.WriteLine("i3dm header magic: " + i3dm.I3dmHeader.Magic);
-            Console.WriteLine("i3dm featuretable json: '" + i3dm.FeatureTableJson+"'");
+            Console.WriteLine("i3dm featuretable json: '" + i3dm.FeatureTableJson + "'");
             Console.WriteLine("i3dm instances length: " + i3dm.FeatureTable.InstancesLength);
             if (o.ShowBatchTableJson)
             {
                 Console.WriteLine("i3dm batch table json: '" + i3dm.BatchTableJson + "'");
             }
-            if (i3dm.FeatureTable.BatchIdOffset!=null && i3dm.FeatureTable.BatchIdOffset.componentType != null)
+            if (i3dm.FeatureTable.BatchIdOffset != null && i3dm.FeatureTable.BatchIdOffset.componentType != null)
             {
                 Console.WriteLine("i3dm batchId component type: " + i3dm.FeatureTable.BatchIdOffset.componentType);
             }
@@ -62,7 +62,7 @@ namespace i3dm.tooling
                 Console.WriteLine("i3dm RTC_CENTER: " + i3dm.FeatureTable.RtcCenter);
             }
             var validationErrors = i3dm.I3dmHeader.Validate();
-            if(validationErrors.Count > 0)
+            if (validationErrors.Count > 0)
             {
                 Console.WriteLine($"Validation check: {validationErrors.Count} errors");
                 foreach (var error in validationErrors)
@@ -75,13 +75,13 @@ namespace i3dm.tooling
                 Console.WriteLine("Validation check: no errors");
             }
 
-            PrintItems<Vector3>(i3dm.Positions, "positions ");
-            PrintItems<Vector3>(i3dm.NormalUps, "normal ups ");
-            PrintItems<Vector3>(i3dm.NormalRights, "normal rights ");
-            PrintItems<Vector3>(i3dm.ScaleNonUniforms, "Scale non-uniform ");
-            PrintItems<float>(i3dm.Scales, "Scales");
+            PrintItems(i3dm.Positions, "positions ");
+            PrintItems(i3dm.NormalUps, "normal ups ");
+            PrintItems(i3dm.NormalRights, "normal rights ");
+            PrintItems(i3dm.ScaleNonUniforms, "Scale non-uniform ");
+            PrintItems(i3dm.Scales, "Scales");
 
-            PrintItems<int>(i3dm.BatchIds, "Batch ids: ");
+            PrintItems(i3dm.BatchIds, "Batch ids: ");
 
             var stream = new MemoryStream(i3dm.GlbData);
             try
@@ -125,8 +125,8 @@ namespace i3dm.tooling
         {
             if (items != null)
             {
-                var extra = items.Count > 5? "(first 5)": String.Empty;
-                
+                var extra = items.Count > 5 ? "(first 5)" : String.Empty;
+
                 var firstfive = items.Take(5);
 
                 Console.WriteLine(name + extra + ": " + string.Join(',', firstfive));
@@ -144,40 +144,26 @@ namespace i3dm.tooling
             var f = File.OpenRead(o.Input);
             var i3dm = I3dmReader.Read(f);
             Console.WriteLine("i3dm version: " + i3dm.I3dmHeader.Version);
-            var stream = new MemoryStream(i3dm.GlbData);
-            try
+            var glbfile = (o.Output == string.Empty ? Path.GetFileNameWithoutExtension(o.Input) + ".glb" : o.Output);
+            var batchfile = (o.Output == string.Empty ? Path.GetFileNameWithoutExtension(o.Input) + ".batch" : o.Output);
+
+            if (File.Exists(glbfile) && !o.Force)
             {
-                var gltf = SharpGLTF.Schema2.ModelRoot.ReadGLB(stream);
-
-                Console.WriteLine("glTF asset generator: " + gltf.Asset.Generator);
-                Console.WriteLine("glTF version: " + gltf.Asset.Version);
-                var glbfile = (o.Output == string.Empty ? Path.GetFileNameWithoutExtension(o.Input) + ".glb" : o.Output);
-                var batchfile = (o.Output == string.Empty ? Path.GetFileNameWithoutExtension(o.Input) + ".batch" : o.Output);
-
-                if (File.Exists(glbfile) && !o.Force)
-                {
-                    Console.WriteLine($"File {glbfile} already exists. Specify -f or --force to overwrite existing files.");
-                }
-                else
-                {
-                    gltf.SaveGLB(glbfile);
-                    File.WriteAllBytes(glbfile, i3dm.GlbData);
-                    Console.WriteLine($"Glb created: {glbfile}");
-                    if (i3dm.BatchTableJson != String.Empty)
-                    {
-                        var sb = new StringBuilder();
-                        sb.Append(i3dm.FeatureTableJson);
-                        sb.AppendLine();
-                        sb.Append(i3dm.BatchTableJson);
-                        File.WriteAllText(batchfile, sb.ToString());
-                        Console.WriteLine($"batch file created: {batchfile}");
-                    }
-                }
+                Console.WriteLine($"File {glbfile} already exists. Specify -f or --force to overwrite existing files.");
             }
-            catch (InvalidDataException ex)
+            else
             {
-                Console.WriteLine("glTF version not supported.");
-                Console.WriteLine(ex.Message);
+                File.WriteAllBytes(glbfile, i3dm.GlbData);
+                Console.WriteLine($"Glb created: {glbfile}");
+                if (i3dm.BatchTableJson != String.Empty)
+                {
+                    var sb = new StringBuilder();
+                    sb.Append(i3dm.FeatureTableJson);
+                    sb.AppendLine();
+                    sb.Append(i3dm.BatchTableJson);
+                    File.WriteAllText(batchfile, sb.ToString());
+                    Console.WriteLine($"batch file created: {batchfile}");
+                }
             }
         }
     }
